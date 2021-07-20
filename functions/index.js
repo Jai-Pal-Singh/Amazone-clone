01c3c89e5627736afb7af7b5efb452cd6bd66da1
Jai Pal Singh <jai.pal.2013@gmail.com>
2021-07-20 08:35:41 +0530

    Configurational changes

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,22 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const secretKey =
-  "sk_test_51JEFjDSJdvKy4mRrcwg9KtHGPzZJ9VzRCfbHEam7vmRlAF7HNI3HIgs6XbmHXgGIMZxJoSH5Q5nDVrY4RzT1royh00BHwsTFei";
-const stripe = require("stripe")(secretKey);
+const secretKey1 = "sk_test_51JEFjDSJdvKy4mRrcwg9KtHGPzZJ9VzRCfbHEam7vmRlAF";
+const secretKey2 = "7HNI3HIgs6XbmHXgGIMZxJoSH5Q5nDVrY4RzT1royh00BHwsTFei";
+const stripe = require("stripe")(secretKey1+secretKey2);
+// import { loadStripe } from "@stripe/stripe-js/pure";
 
+// // Stripe.js will not be loaded until `loadStripe` is called
+// const stripe = await loadStripe(secretKey);
 // => API setup
 
 // - App Config
 const app = express();
 
 // - Middlesware
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
-app.all("/", function (req, res, next) {
+app.all("/", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
@@ -22,7 +25,7 @@ app.all("/", function (req, res, next) {
 // - API Routes
 app.get("/", (req, res, next) => res.status(200).send("hello world"));
 
-var getClientSecret = async (req, res, next) => {
+app.post("/payments/create", async (req, res, next) => {
   const total = req.query.total;
   console.log("Payment request for amount", total);
 
@@ -35,9 +38,7 @@ var getClientSecret = async (req, res, next) => {
   res.status(201).send({
     clientSecret: paymentIntent.client_secret,
   });
-};
-
-app.post("/payments/create", getClientSecret);
+});
 
 // - Listen Command
 exports.api = functions.https.onRequest(app);
